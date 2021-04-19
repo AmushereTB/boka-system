fdcb3c0c62cde2626275619bc38184d58325eb07
Tianbiao Feng <tianbiaofeng@gmail.com>
2021-04-19 14:49:48 +0200

    change table unit style, make the text align in the center of the unit

--- public/calendar.js
+++ public/calendar.js
@@ -9,25 +9,25 @@ Date.prototype.getWeekNumber = function () {
 };
 
 //Global variables
-let currentWeekDay = new Date().getDay();
-let currentMonthDate = new Date().getDate();
-let currentYear = new Date().getFullYear();
-let currentMonth = new Date().getMonth() + 1;
-let stringTime = new Date().toDateString();
 let currentWeek = new Date().getWeekNumber();
 let currentDate = new Date();
 let swedTime = new Date().toLocaleString("en-US", { timeZone: "Europe/Stockholm" });
-let incrementOfWeek = 0;
+let incrementOfDays = 0; //this value calculated by days, everytime when it +-7, it will consider as +-1 week.
 
+//function to show week number(不工作，新的一年无法重置)
+const showWeekNum = () => {
+    document.getElementById('week-number').innerHTML = `vecka ${currentWeek}`;
+};
 
 //function to add table head
-function tableHead() {
-    let th; //table <th>
+let tableHead = () => {
+    let th;
     let tblContent = document.getElementById('tableHeadContent');
     tblContent.innerHTML = ''  //remove existed calendar
-    tblContent.style.backgroundColor = 'red'
-    for (let i = 0; i < 6; i++) { //from monday to saturday
-        let headerValue = addDayToDate(getMondayDate(new Date()), i + incrementOfWeek)
+    tblContent.setAttribute('class', 'text-center');
+    tblContent.setAttribute('style', `background-color: ${"#e6ffff"}`)
+    for (let i = 0; i < 6; i++) {
+        let headerValue = addDayToDate(getMondayDate(new Date()), i + incrementOfDays)
             .toLocaleString('sv-SE', { weekday: 'long', month: 'short', day: '2-digit' });
         th = document.createElement('th');
         th.innerHTML = headerValue;
@@ -36,34 +36,24 @@ function tableHead() {
 };
 
 //add days to certain date and get new date
-function addDayToDate(ymd, ad) { //ymd:date. ad: add days
+let addDayToDate = (ymd, ad) => { //ymd:date. ad: add days
     ymd.setDate(ymd.getDate() + ad);
     return ymd;
 }
 
-
 //function to get Monday's date
-//need to be changed later to dymnatic
-function getMondayDate(d) {
+let getMondayDate = (d) => {
     //d = new Date(d);
     let day = d.getDay();
-    /** d.getDate() to get that day's date
-     * date - day + 1 or +(-6)
-     * if it's not sunday, then + 1, if it's sunday, -6*/
     let mondayDate = d.getDate() - day + (day == 0 ? -6 : 1);
-    /**use setDate to set monday's date as parameter and pass it 
-     * to today's date, then recall new Date
-    */
+    /**  date - day + 1 or +(-6)
+ * if it's not sunday, then + 1, if it's sunday, -6*/
     return new Date(d.setDate(mondayDate));
 }
+
 /**
  * getData & arrayData is using to test purpose
  */
-let getData = fetch('/get_users').then(response => response.json()).then(data => {
-    return Array.isArray(data)
-});
-
-
 const arrayData = async () => {
     const response = await fetch('/get_users');
     const data = await response.json();
@@ -71,23 +61,11 @@ const arrayData = async () => {
 };
 
 
-//from here, it needs another comparison, to compare the date in unit and today's date, if unit date is larger, processed the following 
-
-
-/**下面的比较比的是只要日期比今天大，就顺位把表格CELL加进去,
- * 主要问题是： 日期与格子没有联系
- * 改正方法： (1) 以日期为单位， 7天-今天作为横排的数值，起始值设在当天。 应该从col下手   [可行性太低]
- * (2) 把表格头放到这里，以日期为导向， 当日期与今天的日期相等是CASE1， 日期大于今天CASE2， 日期小于今天CASE3
- * (3) 那UNIT作比较，直接比较现在的时间和UNIT里的时间，小的就加空的 TR TD
- */
-/**先把TABLE的生成变为以先生成COLUMN，然后再生成ROW */
-
-
 //create function to add row to each colmn
 const tableBody = () => {
 
-    let row; //table <tr>
-    let column; //table <td>
+    let row;
+    let column;
     const tblBodyContent = document.getElementById('tableBodyContent');
     const onlyNum = /\D/g;
     tblBodyContent.innerHTML = '' //remove existed calendar
@@ -109,62 +87,48 @@ const tableBody = () => {
                 row = document.createElement('tr');
                 for (let col = 0; col < 6; col++) { //only monday to saturday
                     if (col === 5 && i > 5) break; //saturday stop at 15:00
-                    column = document.createElement('td')
-                    let calendarTime = new Date(addDayToDate(getMondayDate(new Date()), col + incrementOfWeek));
+
+                    let calendarTime = new Date(addDayToDate(getMondayDate(new Date()), col + incrementOfDays));
                     let unitIdValue = `${calendarTime.toLocaleDateString()}, 
                     ${startingHour(col) + i < 10 ? '0' + (startingHour(col) + i) : startingHour(col) + i}`;
                     let unitValue = `${startingHour(col) + i < 10 ? '0' + (startingHour(col) + i) : startingHour(col) + i}:00`
-                    let unitID = unitIdValue.replace(onlyNum, ''); //now have date, need add time
+                    let unitID = unitIdValue.replace(onlyNum, '');
+
+                    column = document.createElement('td')
+                    column.setAttribute('class', 'text-center align-middle');
+                    column.setAttribute('id', unitID);
+
 
                     calendarTime.setHours(startingHour(col) + i < 10 ? '0' + (startingHour(col) + i) : startingHour(col) + i);
                     calendarTime.setMinutes(0);
                     calendarTime.setSeconds(0);
 
-                    //console.log(new Date(swedTime).toLocaleDateString())
-                    let test = new Date(swedTime).toLocaleDateString() > calendarTime.toLocaleDateString();
-                    let testb = new Date(swedTime) > calendarTime
-
-
-                    if (new Date(swedTime).toLocaleDateString() > calendarTime.toLocaleDateString()) {
+                    if (new Date(swedTime) > calendarTime) {
 
                         column.innerHTML = '';
-                        column.setAttribute('id', unitID);
-                        column.setAttribute("style", "visibility: hidden")
+                        column.setAttribute('style', 'visibility: hidden')
                         row.appendChild(column);
 
-                    } else if (new Date(swedTime).getDate() == calendarTime.getDate() && new Date(swedTime) > calendarTime) {
-
-                        column.innerHTML = 'bokad';
-                        //td.set.style.cssText = "background-color: red"
-                        column.setAttribute('id', unitID);
-                        column.setAttribute("style", "background-color:gray")
-                        row.appendChild(column);
+                        if (new Date(swedTime).getDate() == calendarTime.getDate() && new Date(swedTime).getMonth() == calendarTime.getMonth()) {
+                            column.innerHTML = 'bokad';
+                            column.setAttribute('style', 'background-color:gray')
+                            row.appendChild(column);
+                        }
 
                     } else {
-
                         if (data.includes(unitID)) {
                             column.innerHTML = 'bokad';
-                            //td.set.style.cssText = "background-color: red"
-                            column.setAttribute('id', unitID);
-                            column.setAttribute("style", "background-color:gray")
-
+                            column.setAttribute('style', 'background-color:gray')
                         } else {
-
                             column.innerHTML = unitValue;
-                            column.setAttribute('id', unitID);
                             column.addEventListener('click', unitclick);
-
                         }
-                        
+
                         row.appendChild(column);
                     };
-
-
-                    //row.appendChild(column);
                 }
                 tblBodyContent.appendChild(row);
             } //for loop end
-
         })//fetch end
 };
 
@@ -193,7 +157,8 @@ function startingHour(wd) { //weekday
 const leftButton = document.getElementById('previousWeek');
 leftButton.addEventListener('click', minusSevenDays);
 function minusSevenDays() {
-    incrementOfWeek -= 7;
+    incrementOfDays -= 7;
+    currentWeek -= 1;
     creatCalendar();
 }
 
@@ -201,18 +166,16 @@ function minusSevenDays() {
 const rightButton = document.getElementById('nextWeek');
 rightButton.addEventListener('click', addSevenDays);
 function addSevenDays() {
-    incrementOfWeek += 7;
+    incrementOfDays += 7;
+    currentWeek += 1;
     creatCalendar();
 }
 
 function creatCalendar() {
-    tableHead(); //call table head
-    tableBody(); //call table body
-
+    showWeekNum();
+    tableHead();
+    tableBody();
 }
 
 creatCalendar()
 
-//a function: if time has passed, the old table block won't show
-//a function: any booked block can not be hover or clicked
-//a function: use today's hour compair to canlendar, if time is passed, lock the schedule
